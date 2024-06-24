6cb4b72c2aaf7dae44da439a38ad2a3805f23111
om pharate <72200400+ompharate@users.noreply.github.com>
2024-06-24 12:44:19 +0000

    path moudle

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-const utils = require("./utils/index")
-utils.add(10,20)
-console.log(utils.name);
+const path = require("path")
+
+const fileName = path.basename("./utils/index.js")
+console.log(fileName)
+
+console.log(path.extname("./utils/index.js"))
+
+const path3 = path.join("abc", "", "", "deatils.txt");
+console.log(path3)
+
+console.log("Current directory:", __dirname);
